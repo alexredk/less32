e3a96b975c40be64de77c33dc9c7f64cd136fe2e
Student7 <Student7@student.com>
2021-02-22 17:53:25 +0200

    a

---
+++ main.js
@@ -0,0 +1,42 @@
+// let x = ""+1+0
+// console.log(x)
+// let y = true+false
+// console.log(y)
+// let z = 6/"3"
+// console.log(z)
+// let d = 4+5+"px"
+// console.log(d)
+// let w = "px"+5+4
+// console.log(w)
+// let q = "4"-2
+// console.log(q)
+// let a = Number(prompt("Введите первое число"))
+// let b = Number(prompt("Введите второе число"))
+// let c = a+b
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// let a = prompt("Число")
+//   if(a==10)
+//   alert("Юля молодец")
+//   else if (a==7){
+//         alert("Молодец")}
+//       else
+ 
+    //   let a = prompt("Введите свой возраст")
+    //   if (a==0)
+    //   alert ("Вы Дошкольник")
+    //   else(a==6)
+    //   alert ("Вы тоже дошкольник")
+
+    let a = prompt("В каком году бфл создан первый сценарий JS")
+    if (a==2015){
+        alert ("Вы правы")}
+    
+else if(a < 2015){
+    alert ("Вы немного ошиблись")}
+
+ else if(a > 2015){
+     alert ("вы немного не правы")}
+     
+  
